5e9093c614c668dc1a4b71e7204441c4ea0c4f8d
John O <johnolivas7553@gmail.com>
2022-05-16 11:32:16 -0700

    Camera
    A script that controls the camera has been added.

--- Assets/Scripts/soldierSpawner.cs
+++ Assets/Scripts/soldierSpawner.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class soldierSpawner : MonoBehaviour
 {
     public float speed = 0;
+    public Transform ground;
     public Transform prince;
+    public Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = prince.position;
+        transform.position = ground.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = prince.position + offset;
     }
 }
